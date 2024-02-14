ee0f824a5c30c6e6792141c3ca1f6cd979970504
aimerie <kaitebug@gmail.com>
2024-02-14 01:16:23 -0700

    initial setup, package config

--- AminalsWeb/App_Start/BundleConfig.cs
+++ AminalsWeb/App_Start/BundleConfig.cs
@@ -9,21 +9,17 @@ namespace AminalsWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/node_modules/jquery/dist/jquery.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/node_modules/bootstrap/dist/js/bootstrap.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                "~/node_modules/knockout/build/output/knockout-latest.debug.js",
+                "~/Scripts/Custom/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/node_modules/bootstrap/dist/css/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
